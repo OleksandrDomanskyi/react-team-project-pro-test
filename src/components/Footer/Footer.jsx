91616777dd2cd6b6516202792d230e3ac65e6e8a
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-06-15 02:40:17 +0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+
+import styles from './footer.module.scss';
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <section className="container">
+                <div className={styles.footer}>
+                    <p className={styles.text}>&#169; 2022 &#124; All Rights Reserved &#124; Developed with</p>
+                    <p className={styles.text}>by</p>
+                    <a className={styles.link} href="https://goit.ua/" target="_blank">Go
+                        <span className={styles.item}>IT</span>
+                    </a>
+                    <Link to='/contacts' className={styles.contacts}>Students</Link>
+                </div>
+            </section>
+        </footer>
     )
 };
 
--- src/components/SelectTest/SelectTest.jsx
+++ src/components/SelectTest/SelectTest.jsx
@@ -4,18 +4,20 @@ import styles from './select-test.module.scss';
 
 const SelectTest = () => {
     return (
-        <section className='container'>
-            <div className={styles.select}>
-                <h2 className={styles.title}>“Regression testing. What is it? <br/>
-If the system compiles, that's good, if it boots, that's great!”</h2>
-                <p className={styles.name}>Linus Torvalds</p>
-                <p className={styles.text}>Linux kernel creator, hacker, 1969</p>
-                <div className={styles.choice}>
-                    <Link to='/test' className={styles.link}>QA technical training</Link>
-                    <Link to='/test' className={styles.link}>Testing theory</Link>
+        <main>
+            <section className='container'>
+                <div className={styles.select}>
+                    <h2 className={styles.title}>“Regression testing. What is it? <br/>
+    If the system compiles, that's good, if it boots, that's great!”</h2>
+                    <p className={styles.name}>Linus Torvalds</p>
+                    <p className={styles.text}>Linux kernel creator, hacker, 1969</p>
+                    <div className={styles.choice}>
+                        <Link to='/test' className={styles.link}>QA technical training</Link>
+                        <Link to='/test' className={styles.link}>Testing theory</Link>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </main>
     )
 };
 
