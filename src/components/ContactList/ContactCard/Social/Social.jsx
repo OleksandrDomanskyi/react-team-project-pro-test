8aa61d02151965cf070ed6f278e40273edd2c7df
Myroslav Vecheruk <mvecheruk@gmail.com>
2022-06-15 12:12:08 +0300

    created icons components

--- src/components/ContactList/ContactCard/Social/Social.jsx
+++ src/components/ContactList/ContactCard/Social/Social.jsx
@@ -1,40 +1,24 @@
-import Icon from "../../../../shared/components/Icon";
 import s from "./social.module.scss";
+import Linkedin from "./SocialLogo/Linkedin";
+import Mail from "./SocialLogo/Mail";
+import Github from "./SocialLogo/Github";
 
 const Social = ({ social: { github, linkedin, mail } }) => {
   return (
     <ul className={s.list}>
       <li className={s.item}>
         <a href={github} rel="noopener noreferrer" target="_blank">
-          <Icon
-            className={s.icon}
-            name="icon-github"
-            width="25"
-            height="25"
-            color={"#000"}
-          />
+          <Github className={s.icon} width="25" height="25" fill="black" />
         </a>
       </li>
       <li className={s.item}>
         <a href={linkedin} rel="noopener noreferrer" target="_blank">
-          <Icon
-            className={s.icon}
-            name="icon-linkedin"
-            width="25"
-            height="25"
-            color={"#000"}
-          />
+          <Linkedin className={s.icon} width="25" height="25" fill="black" />
         </a>
       </li>
       <li className={s.item}>
         <a href={mail} rel="noopener noreferrer" target="_blank">
-          <Icon
-            className={s.icon}
-            name="icon-mail"
-            width="25"
-            height="25"
-            color={"#000"}
-          />
+          <Mail className={s.icon} width="25" height="25" fill="black" />
         </a>
       </li>
     </ul>
