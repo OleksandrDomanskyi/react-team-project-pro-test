461ad89860d4a51f0b4278f5ba15a74331b8f1c2
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-06-15 13:22:20 +0300

    quick-fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <div className={styles.footer}>
                     <p className={styles.text}>&#169; 2022 &#124; All Rights Reserved &#124; Developed with</p>
                     <p className={styles.text}>by</p>
-                    <a className={styles.link} href="https://goit.ua/" target="_blank">Go
+                    <a className={styles.link} href="https://goit.ua/" target="_blank" rel="noreferrer">Go
                         <span className={styles.item}>IT</span>
                     </a>
                     <Link to='/contacts' className={styles.contacts}>Students</Link>
