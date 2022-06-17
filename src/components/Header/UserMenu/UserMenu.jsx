181dc4c35e3c9d80c55240eab64ac45ba1f3b97e
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-06-17 21:36:46 +0200

    some stuff

--- src/components/Header/UserMenu/UserMenu.jsx
+++ src/components/Header/UserMenu/UserMenu.jsx
@@ -6,7 +6,14 @@ const UserMenu = ({ isMobile }) => {
   const name = "oleksii.kikot@yahoo.com";
   return (
     <div className={styles.userWrapper}>
-      <p className={styles.userName}>{name.slice(0, 1)}</p>
+      <p className={styles.userName}>
+        <img
+          className={styles.userName}
+          src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png"
+          // https://cdn.pixabay.com/photo/2020/10/21/04/12/man-5672061_1280.png
+          alt="avatar"
+        />
+      </p>
       {!isMobile && <p className={styles.fullName}>{name}</p>}
     </div>
   );
