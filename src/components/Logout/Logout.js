a072cedf8b306a978010f41f6d5bac41f4e1c544
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-06-17 22:10:39 +0300

    Setup login, logout operations

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { GoogleLogout } from "react-google-login";
+
+const clientId =
+  "707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com";
+
+function Logout() {
+  const onSuccess = () => {
+    console.log("Logout made successfully");
+    alert("Logout made successfully ✌");
+  };
+
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      ></GoogleLogout>
+    </div>
+  );
+}
+
+export default Logout;
