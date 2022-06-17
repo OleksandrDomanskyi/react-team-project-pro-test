cad716e8d7296af146f3db8376c238a304bdb845
MawkaA <94409557+MawkaA@users.noreply.github.com>
2022-06-17 00:18:00 -0400

    styles
    added lines

--- src/components/UsefulInfo/UsefulInfo.jsx
+++ src/components/UsefulInfo/UsefulInfo.jsx
@@ -6,14 +6,14 @@ const UsefulInfo = () => {
     const literatureList = literature.map(item => {
         return (
           <li className={styles.literatureItem} key={nanoid()}>
-            <a href={item.link}>{item.content}</a>
+            <a className={styles.literatureLink}  href={item.link}>{item.content}</a>
           </li>
         );
       });
       const resourcesList = resources.map(item => {
         return (
           <li className={styles.resourceItem} key={nanoid()}>
-            <a href={item.link}>{item.content}</a>
+            <a className={styles.resourceLink} href={item.link}>{item.content}</a>
           </li>
         );
       });
@@ -23,10 +23,10 @@ const UsefulInfo = () => {
         <div className={styles.block}>
           <h2 className={styles.title}>Useful literature</h2>
           {/* <img alt='line' src="../../images/icons.svg#line1" width="321" height="1"/> */}
-          <div className={styles.line}></div>
+          <div className={styles.lineTop}></div>
           <ol className={styles.literatureList}>{literatureList}</ol>
           <h2 className={styles.title}>Useful resources</h2>
-          <div className={styles.line}></div>
+          <div className={styles.lineBottom}></div>
           <ol className={styles.resourcesList}>{resourcesList}</ol>
         </div>
         </section>
