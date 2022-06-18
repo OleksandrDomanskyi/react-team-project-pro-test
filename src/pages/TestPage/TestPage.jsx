144b2c1cc1ad7e3b2319f2706ee0ca8d73a49a56
uliakhom <ulianakhomenko@gmail.com>
2022-06-18 19:16:52 +0300

    scss for test_component

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,12 +1,11 @@
 import Test from "../../components/Test";
 
 const TestPage = () => {
-    return (
-        <div>
-            <h2>Test</h2>
-            <Test />
-        </div>
-    )
+  return (
+    <div>
+      <Test />
+    </div>
+  );
 };
 
 export default TestPage;
